612f8fab2d1c479bec1537556e7046b07dff8eb8
Pedro Victor <pedrovictor2003@gmail.com>
2026-02-18 18:38:21 -0300

    cria aula 04 sobre operadores

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        double numero02 = 20;
+        double resultado = numero01 + numero02;
+        System.out.println(numero01 - numero02);
+        System.out.println("Valor " + resultado);
+        System.out.println("Valor " + numero01 * numero02);
+        System.out.println("Valor " + numero01 / numero02);
+    }
+}
