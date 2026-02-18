85d77087374ee2a07a4810c25d4c803ac16f9b4f
Pedro Victor <pedrovictor2003@gmail.com>
2026-02-18 18:49:40 -0300

    adiciona exemplos de operadores primarios (boolean e resto)

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -10,5 +10,20 @@ public class Aula04Operadores {
         System.out.println("Valor " + resultado);
         System.out.println("Valor " + numero01 * numero02);
         System.out.println("Valor " + numero01 / numero02);
+
+        // %
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezDiferenteQueVinte = 10 != 20;
+        boolean isDezIgualQueVinte = 10 == 20;
+        System.out.println("isDezMaiorQueVinte: " + isDezMaiorQueVinte);
+        System.out.println("isDezIgualQueVinte: " + isDezIgualQueVinte);
+        System.out.println("isDezMenorQueVinte: " + isDezMenorQueVinte);
+        System.out.println("isDezDiferenteQueVinte: " + isDezDiferenteQueVinte);
     }
 }
