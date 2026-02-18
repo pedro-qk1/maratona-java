9205dad4ace4632bbb5809b987aff21b58f3a99d
Pedro Victor <pedrovictor2003@gmail.com>
2026-02-18 12:41:49 -0300

    primeiro commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    // isso é um comentário de uma linha
+
+    /*
+    isso é um comentário
+    de múltiplas
+    linhas
+     */
+
+    /**
+     * isso é um comentário <b>javadoc</b> com /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        // int, double, float, char, byte, short, long, boolean
+        int idade = (int) 10000000000D;
+        long numeroGrande = (long) 100000.0;
+        double salarioDouble = 2000D;
+        float salarioFloat = (float) 2500D;
+        byte idadeByte = 10;
+        short idadeShorte = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+        System.out.println("A idade é " + idade);
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
+        System.out.println(idade);
+        System.out.println(numeroGrande);
+    }
+}
