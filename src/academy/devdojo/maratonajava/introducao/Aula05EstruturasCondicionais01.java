bdb62d3a00e7ba171f3d5f1da3724ad2d12afdbd
Pedro Victor <pedrovictor2003@gmail.com>
2026-02-19 19:16:08 -0300

    adiciona aula 05.2 sobre else if e else

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,11 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 10;
         boolean isAutorizadoComprarBebida = idade >= 18;
+
         if(isAutorizadoComprarBebida){
             System.out.println("AUTORIZADO a comprar bebida alcoolica.");
+        } else {
+            System.out.println("NÃO AUTORIZADO a comprar bebida alcoolica.");
         }
 
         // ! (NOT)
