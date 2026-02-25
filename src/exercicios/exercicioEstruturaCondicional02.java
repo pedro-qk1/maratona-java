fe5996078af9f46af74444890d7c0f57cee80011
Pedro Victor <pedrovictor2003@gmail.com>
2026-02-25 19:20:29 -0300

    correção no ex2 medio de estrutura condicional

--- src/exercicios/exercicioEstruturaCondicional02.java
+++ src/exercicios/exercicioEstruturaCondicional02.java
@@ -2,16 +2,16 @@ package exercicios;
 
 public class exercicioEstruturaCondicional02 {
     public static void main(String[] args) {
-        int primeiroNumero = -18;
-        int segundoNumero = -11;
-        int terceiroNumero = -10;
+        int primeiroNumero = 110;
+        int segundoNumero = 20;
+        int terceiroNumero = 45;
         int maiorNumero = terceiroNumero;
 
         if(primeiroNumero > maiorNumero){
             maiorNumero = primeiroNumero;
-        } else if(segundoNumero > maiorNumero){
+        } if(segundoNumero > maiorNumero){
             maiorNumero = segundoNumero;
-        } else{
+        } if(terceiroNumero > maiorNumero){
             maiorNumero = terceiroNumero;
         }
         System.out.println("O maior número é: " + maiorNumero);
