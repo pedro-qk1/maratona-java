43023d6f65523aa259ee5336a6ad079ed72a86d3
Pedro Victor <pedrovictor2003@gmail.com>
2026-02-27 18:42:28 -0300

    criação do pacote para POO e introdução a classes em OO

--- src/exercicios/exercicioEstruturaCondicional03.java
+++ src/exercicios/exercicioEstruturaCondicional03.java
@@ -2,45 +2,29 @@ package exercicios;
 
 public class exercicioEstruturaCondicional03 {
     public static void main(String[] args) {
-        double salario = 200000;
-        String impostoUm = "5%";
-        String impostoDois = "15%";
-        String impostoTres = "25%";
-        String impostoQuatro = "35%";
+        double salario = 150000;
+        double imposto = 0;
+        double excedente = 0;
 
+        System.out.println("Salário: R$" + salario);
         if(salario <= 25000){
-            System.out.println("R$" + salario + " paga " + impostoUm);
-        }
-        else if(salario > 25000 && salario <= 50000){
-            if(salario == 50000){
-                System.out.println("R$25000 paga " + impostoUm);
-                System.out.println("R$25000 paga " + impostoDois);
-                System.out.println("Total de impostos sobre o salário: 20%.");
-            } else{
-                System.out.println("R$25000 paga " + impostoUm);
-                System.out.println("R$" + (salario - 25000) + " paga " + impostoDois);
-                System.out.println("Total de impostos sobre o salário: 20%.");
-            }
-        }
-        else if(salario > 50000 && salario <= 100000){
-            if(salario == 100000){
-                System.out.println("R$25000 paga " + impostoUm);
-                System.out.println("R$25000 paga " + impostoDois);
-                System.out.println("R$50000 paga " + impostoTres);
-                System.out.println("Total de impostos sobre o salário: 45%.");
-            } else{
-                System.out.println("R$25000 paga " + impostoUm);
-                System.out.println("R$25000 paga " + impostoDois);
-                System.out.println("R$" + (salario - 50000) + " paga " + impostoTres);
-                System.out.println("Total de impostos sobre o salário: 45%.");
-            }
-        }
-        else{
-            System.out.println("R$25000 paga " + impostoUm);
-            System.out.println("R$25000 paga " + impostoDois);
-            System.out.println("R$50000 paga " + impostoTres);
-            System.out.println("R$" + (salario - 100000) + " paga " + impostoQuatro);
-            System.out.println("Total de impostos sobre o salário: 80%.");
+            imposto = salario * ((double)5/100);
+            System.out.println("PRIMEIRA FAIXA: Imposto sobre salário: 5% (R$" + imposto + ").");
+        } if(salario > 25000 && salario <= 50000){
+            excedente = salario - 25000;
+            System.out.println("PRIMEIRA FAIXA: Imposto sobre salário: R$25000 paga 5% (R$" + (25000 * ((double) 5 / 100)) + ").");
+            System.out.println("SEGUNDA FAIXA: Imposto sobre salário: R$" + excedente + " paga 15% (R$" + (excedente * ((double) 15 / 100)) + ").");
+        } if(salario > 50000 && salario <= 100000){
+            System.out.println("PRIMEIRA FAIXA: Imposto sobre salário: R$25000 paga 5% (R$" + (25000 * ((double) 5 / 100)) + ").");
+            System.out.println("SEGUNDA FAIXA: Imposto sobre salário: R$25000 paga 15% (R$" + (25000 * ((double) 15 / 100)) + ").");
+            excedente = salario - 50000;
+            System.out.println("TERCEIRA FAIXA: Imposto sobre salário: R$" + excedente + " paga 25% (R$" + (excedente * ((double) 25 / 100)) + ").");
+        } if(salario > 100000){
+            System.out.println("PRIMEIRA FAIXA: Imposto sobre salário: R$25000 paga 5% (R$" + (25000 * ((double) 5 / 100)) + ").");
+            System.out.println("SEGUNDA FAIXA: Imposto sobre salário: R$25000 paga 15% (R$" + (25000 * ((double) 15 / 100)) + ").");
+            System.out.println("TERCEIRA FAIXA: Imposto sobre salário: R$50000 paga 25% (R$" + (50000 * ((double) 25 / 100)) + ").");
+            excedente = salario - 100000;
+            System.out.println("QUARTA FAIXA: Imposto sobre salário: R$" + excedente + " paga 35% (R$" + ((excedente * (double)35 / 100)));
         }
     }
 }
