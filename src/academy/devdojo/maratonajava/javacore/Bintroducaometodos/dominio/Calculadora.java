ae3be52ac8a5805b229effd320ce91cb73c01014
Pedro Victor <pedrovictor2003@gmail.com>
2026-03-03 21:54:55 -0300

    criação de novo pacote para o estudo de metodos

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+    public void somarDoisNumeros(){
+        System.out.println(10+10);
+    }
+
+    public void subtrairDoisNumeros(){
+        System.out.println(10-20);
+    }
+}
